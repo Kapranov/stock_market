4c5ec3b1c211f678c5db937bfabcc2874d63df05
Oleg G.Kapranov <lugatex@yahoo.com>
2018-12-18 19:08:26 +0200

    added packages in Master

--- apps/master/mix.exs
+++ apps/master/mix.exs
@@ -11,23 +11,40 @@ defmodule Master.MixProject do
       lockfile: "../../mix.lock",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.json": :test
+      ],
       deps: deps()
     ]
   end
 
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: applications(Mix.env),
       mod: {Master.Application, []}
     ]
   end
 
   defp deps do
     [
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10.1", only: :test},
+      {:ex_unit_notifier, "~> 0.1.4", only: :test},
+      {:mix_test_watch, "~> 0.9.0", only: :dev, runtime: false},
+      {:remix, "~> 0.0.2", only: :dev},
+      {:ex_doc, "~> 0.19.1", only: :dev},
       {:ger_market, in_umbrella: true},
       {:usa_market, in_umbrella: true},
       {:converter, in_umbrella: true},
       {:my_uk_app, in_umbrella: true}
     ]
   end
+
+  defp applications(:dev), do: applications(:all) ++ [:remix]
+  defp applications(_all), do: [:logger]
 end
