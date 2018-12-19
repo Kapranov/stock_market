22231ab28f8ac469d27dc33c30674d5dc2708599
Oleg G.Kapranov <lugatex@yahoo.com>
2018-12-19 13:08:34 +0200

    updated - apps/my_uk_app/lib/my_uk_app/receive_consumer.ex

--- apps/my_uk_app/lib/my_uk_app/receive_consumer.ex
+++ apps/my_uk_app/lib/my_uk_app/receive_consumer.ex
@@ -1,3 +1,27 @@
 defmodule MyUkApp.ReceiveConsumer do
   @moduledoc false
+
+  use GenStage
+
+  use UmbrellaStage,
+    type: :consumer,
+    producers: [
+      {Converter.ReceiveProducerConsumer, []}
+    ]
+
+  @name __MODULE__
+
+  def start_link(_opts) do
+    GenStage.start_link(@name, :ok, name: @name)
+  end
+
+  def init(:ok) do
+    umbrella_sync_subscribe()
+    {:consumer, :nothing}
+  end
+
+  def handle_events(events, _from, state) do
+    Enum.each(events, &Shared.Interface.process_info(MyUkAppInterface, &1))
+    {:noreply, [], state}
+  end
 end
