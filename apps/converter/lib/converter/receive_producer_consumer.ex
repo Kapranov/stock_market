0e8ca071630483da828a482c07adebd5595b899e
Oleg G.Kapranov <lugatex@yahoo.com>
2018-12-19 20:24:30 +0200

    updated - apps/converter/lib/converter/receive_producer_consumer.ex

--- apps/converter/lib/converter/receive_producer_consumer.ex
+++ apps/converter/lib/converter/receive_producer_consumer.ex
@@ -1,3 +1,48 @@
 defmodule Converter.ReceiveProducerConsumer do
   @moduledoc false
+
+  use GenStage
+
+  use UmbrellaStage,
+    type: :producer_consumer,
+    producers: [
+      {GerMarket.ReceiveProducer, []},
+      {UsaMarket.ReceiveProducer, []}
+    ]
+
+  @name __MODULE__
+  @eur_to_gbp 0.90
+  @usd_to_gbp 0.75
+
+  def start_link(_opts), do: GenStage.start_link(@name, :ok, name: @name)
+
+  def init(:ok) do
+    umbrella_sync_subscribe()
+    {:producer_consumer, :nothing}
+  end
+
+  def handle_events(events, _from, state) do
+    converted = Enum.map(events, &convert_to_gbp/1)
+    {:noreply, converted, state}
+  end
+
+  defp convert_to_gbp(
+    %{price_per_share: pps, currency: :eur} = event
+  ) do
+    %{
+      event |
+      price_per_share: round(pps * @eur_to_gbp),
+      currency: :gbp
+    }
+  end
+
+  defp convert_to_gbp(
+    %{price_per_share: pps, currency: :usd} = event
+  ) do
+    %{
+      event |
+      price_per_share: round(pps * @usd_to_gbp),
+      currency: :gbp
+    }
+  end
 end
