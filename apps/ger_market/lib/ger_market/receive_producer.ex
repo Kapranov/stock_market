ada5fa13c41c5e28de27b672278da8f78857da87
Oleg G.Kapranov <lugatex@yahoo.com>
2018-12-19 21:05:18 +0200

    updated - apps/ger_market/lib/ger_market/receive_producer.ex

--- apps/ger_market/lib/ger_market/receive_producer.ex
+++ apps/ger_market/lib/ger_market/receive_producer.ex
@@ -1,3 +1,27 @@
 defmodule GerMarket.ReceiveProducer do
   @moduledoc false
+
+  use GenStage
+
+  use UmbrellaStage,
+    type: :producer
+
+  @name __MODULE__
+
+  def start_link(_opts), do: GenStage.start_link(@name, :ok, name: @name)
+
+  def receive_info(event), do: GenStage.call(@name, {:receive_info, event})
+
+  def init(:ok) do
+    umbrella_sync_subscribe()
+    {:producer, :nothing}
+  end
+
+  def handle_call({:receive_info, event}, _from, state) do
+    {:reply, :ok, [event], state}
+  end
+
+  def handle_demand(_demand, state) do
+    {:noreply, [], state}
+  end
 end
