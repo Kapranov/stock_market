66c685aa6046adee12cba853a4de1e1a70763569
Oleg G.Kapranov <lugatex@yahoo.com>
2018-12-19 12:54:27 +0200

    updated - apps/shared/lib/shared/interface.ex

--- apps/shared/lib/shared/interface.ex
+++ apps/shared/lib/shared/interface.ex
@@ -1,3 +1,22 @@
 defmodule Shared.Interface do
   @moduledoc false
+
+  use GenServer
+
+  @name __MODULE__
+
+  def start_link(name, test_pid) do
+    GenServer.start_link(@name, test_pid, name: name)
+  end
+
+  def process_info(name, event) do
+    GenServer.call(name, {:process_info, event})
+  end
+
+  def init(test_pid), do: {:ok, test_pid}
+
+  def handle_call({:process_info, event}, _from, test_pid) do
+    send(test_pid, {:received, event})
+    {:reply, :ok, test_pid}
+  end
 end
