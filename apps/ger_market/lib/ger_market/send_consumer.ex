c8cca8bac0f9de58d1cff52e7fe40dc9fdf7f21e
Oleg G.Kapranov <lugatex@yahoo.com>
2018-12-19 21:09:48 +0200

    updated - apps/ger_market/lib/ger_market/send_consumer.ex

--- apps/ger_market/lib/ger_market/send_consumer.ex
+++ apps/ger_market/lib/ger_market/send_consumer.ex
@@ -1,3 +1,26 @@
 defmodule GerMarket.SendConsumer do
   @moduledoc false
+
+  use GenStage
+
+  use UmbrellaStage,
+    type: :consumer,
+    producers: [{
+        Converter.SendProducerConsumer,
+        [selector: fn(%{currency: currency}) -> currency == :eur end]
+      }]
+
+  @name __MODULE__
+
+  def start_link(_opts), do: GenStage.start_link(@name, :ok, name: @name)
+
+  def init(:ok) do
+    umbrella_sync_subscribe()
+    {:consumer, :nothing}
+  end
+
+  def handle_events(events, _from, state) do
+    Enum.each(events, &Shared.Interface.process_info(GerMarketInterface, &1))
+    {:noreply, [], state}
+  end
 end
