49c54bc4b7822c9c1ff27e9366bc73240e47d54c
Oleg G.Kapranov <lugatex@yahoo.com>
2018-12-20 13:28:58 +0200

    updated added function generate - Shared.DataGenerator

--- apps/shared/lib/shared/data_generator.ex
+++ apps/shared/lib/shared/data_generator.ex
@@ -1,3 +1,25 @@
 defmodule Shared.DataGenerator do
   @moduledoc false
+
+  @steps 1..1000
+
+  def generate do
+    for _x <- (@steps) do
+      [{"US", :usd}, {"GER", :eur}, {"UK", :gbp}]
+      |> Enum.random()
+      |> generate()
+    end
+  end
+
+  defp generate({"US", _} = data) do
+    data
+  end
+
+  defp generate({"GER", _} = data) do
+    data
+  end
+
+  defp generate({"UK", _} = data) do
+    data
+  end
 end
